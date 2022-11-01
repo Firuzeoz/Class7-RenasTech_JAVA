5a174d759b1a2607d1f78502a6698ef4034808b9
Firuze OZ <firuzoz@gmail.com>
2022-11-01 02:31:05 -0400

    Last 2 Classes2

--- src/Day16_Arrays/Topic3_AssigningValuestoArrays.java
+++ src/Day16_Arrays/Topic3_AssigningValuestoArrays.java
@@ -4,7 +4,39 @@ public class Topic3_AssigningValuestoArrays {
     public static void main(String[] args) {
 
         //
-        int[] intArray={98,23,4,5,1,2};
+        int[] intArray={98,23,4,5,1,2};//duplicate values are allowed in Arrays
+        // getting the value in index 4
+        int val=intArray[4];
+        System.out.println("The value in index 4 or in var variable: " + val);
+
+        System.out.println("***********************");
+        int [] array1=new int[4]; // {0,0,0,0}
+        array1[0]=78; //hey change the number of index 0 ,assign it 78 value
+        System.out.println("The value in index 0 is : " +array1[0]);
+        System.out.println("The value in index 1 is : " +array1[1]);
+        array1[1]=90;
+        System.out.println("The value in index 1 is : " +array1[1]);
+        array1[2]=76;
+        array1[3]=12;
+        // array1[4]=79; we don't have an index 4 , this will result an exception of ArrayIndexOutOfBound
+        //array1={78,90,76,12}
+
+        System.out.println("***************");
+        char[]charArray=new char[5];
+        charArray[0]='d';
+        charArray[1]='c';
+        charArray[2]=' ';
+        charArray[3]='o';
+        charArray[4]='2';  //{'d','c',' ','o','2'}
+        System.out.println("The char at index 0 is: "+charArray[0]);
+        System.out.println("The char at index 1 is: "+charArray[1]);
+        System.out.println("The char at index 2 is: "+charArray[2]);
+        System.out.println("The char at index 3 is: "+charArray[3]);
+        System.out.println("The char at index 4 is: "+charArray[4]);
+
+
+
+
 
 
     }
---
+++ src/Day16_Arrays/Topic6_ArrayExample.java
@@ -0,0 +1,11 @@
+package Day16_Arrays;
+
+public class Topic6_ArrayExample {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
